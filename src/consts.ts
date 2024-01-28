ba390ed74dbf93e1d18375457b223c33145c9047
anderspk <kamsvaag.anders@gmail.com>
2024-01-28 03:13:03 +0100

    Add initial project files

---
+++ src/consts.ts
@@ -0,0 +1,16 @@
+export const PROJECTS = [
+  {
+    title: 'Chrome Tab Manager',
+    description:
+      'A Chrome extension that allows you to manage your tabs and tab-groups in a more efficient way.',
+    devices: ['iPhone', 'iPad', 'Android'],
+    status: 'In Progress',
+  },
+  {
+    title: 'Alt+Tab',
+    description:
+      'An alt+tab replacement for Windows that groups windows by application',
+    devices: ['iPhone', 'iPad', 'Android'],
+    status: 'In Progress',
+  },
+];
